401c789a81ed6be832f1f562131827a2efef4ad1
yuzhun20-web <yuzhun20@gmail.com>
2025-09-18 23:36:54 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('csv-ro-cache-fix2').then(c => c.addAll([
+    './','./index.html','./style.css','./app.js','./manifest.webmanifest'
+  ])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
